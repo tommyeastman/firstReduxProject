fd53588f86a686e99044a3144b526aa66bbe9487
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-28 00:16:11 -0300

    tried copy/pasting some of his code.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { CardSection } from './common';
 
-class ListItem extends React.Component {
-    render() {
-        return (
-            <CardSection>
-                <Text>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
-        );
-    }
+class ListItem extends Component {
+  render() {
+    const { titleStyle } = styles;
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    );
+  }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
